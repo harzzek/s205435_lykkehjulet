c8b5c83dde2f43dcb847efcd2d77b39e2d75ba85
harzzek <39692510+harzzek@users.noreply.github.com>
2021-11-24 20:54:04 +0100

    BAM forstår ingenting

---
+++ app/src/main/java/com/example/s205435lykkehjulet/CategoryWords.kt
@@ -0,0 +1,7 @@
+package com.example.s205435lykkehjulet
+
+enum class CategoryWords(val listOf: List<String>) {
+    BILMAERKER(listOf("Audi", "Mercedes", "Bmw", "Kia")),
+    MAD(listOf("Lasagne", "Pizza", "Sandwich", "Ananas")),
+    COMPUTERSPIL(listOf("Battlefield", "Fifa", "Fortnite","Minecraft"))
+}
